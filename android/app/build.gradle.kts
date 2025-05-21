f82a89c555b537670b0240b193e950227f6f07a8
malachieborohoul <malachieborohoul@gmail.com>
2025-05-21 02:33:54 +0100

    update build file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,11 @@ val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}else {
+    keystoreProperties["storePassword"] = System.getenv("STORE_PASSWORD") ?: ""
+    keystoreProperties["keyPassword"] = System.getenv("KEY_PASSWORD") ?: ""
+    keystoreProperties["keyAlias"] = System.getenv("KEY_ALIAS") ?: ""
+    keystoreProperties["storeFile"] = System.getenv("KEYSTORE_FILE") ?: ""
 }
 
 android {
@@ -43,7 +48,7 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
